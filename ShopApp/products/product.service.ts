f8689529e8afe738a258ff8239e83758ebf95959
abdallah-khalil <abdallah.khalil.nada@gmail.com>
2018-12-11 07:49:09 +0200

    first commit

--- ShopApp/products/product.service.ts
+++ ShopApp/products/product.service.ts
@@ -5,7 +5,6 @@ export class ProductService {
     static $inject: string[] = ['$http'];
     constructor(private $http: angular.IHttpService) {
         'ngInject';
-
     }
     getProducts() {
         return this.$http.get('/Assets/jsonData/products.json').then(response =>  response.data );
